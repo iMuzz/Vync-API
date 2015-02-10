f352eae13bbeb56c18dcf30067198c868e371951
tsabend <tsabend@gmail.com>
2015-02-10 12:35:51 -0500

    Change user model for facebook integration

--- db/migrate/20141107113140_create_users.rb
+++ db/migrate/20141107113140_create_users.rb
@@ -4,6 +4,11 @@ class CreateUsers < ActiveRecord::Migration
       t.string :device_id
       t.string :devicetoken
       t.string :username
+      # t.string :phone_number
+      t.string :email
+      t.string :facebook_object_id
+      # t.string :facebook_avatar_url
+
       t.datetime :created_at
     end
   end
