4a84b350fe84edac2616bb9a494a9d2896df7ac2
tsabend <tsabend@gmail.com>
2015-02-10 12:36:35 -0500

    Remove unused relationships add email validation for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :sent_messages, class_name: "VideoMessage", foreign_key: :sender_id
   has_many :received_messages, class_name: "VideoMessage", foreign_key: :recipient_id
-  has_many :finished_chains, through: :video_messages
-  has_many :received_notifications
-  has_many :chain_updated_notifications
-  # validates :device_id, uniqueness: true
+  validates :email, uniqueness: true
 
   def all_messages(since = 0)
     # temporarily ignoring since because it's giving off by one errors.
