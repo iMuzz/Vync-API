5005fc2bd3a9a863f7bb9534ef23fd6056432f5c
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-03-20 07:03:29 -0500

    fixed footer styling

--- app/models/video_message.rb
+++ app/models/video_message.rb
@@ -30,7 +30,7 @@ class VideoMessage < ActiveRecord::Base
   end
 
   def source
-    "https://s3-us-west-2.amazonaws.com/telephono/#{self.video_id}"
+    "https://s3-us-west-2.amazonaws.com/telephono/#{self.video_id}.mov"
   end
 
   def get_sources
